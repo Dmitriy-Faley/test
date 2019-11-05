9ac386ed5a6f3d553893ef0398151f23a18430f3
Dmitriy <dima.faley.98@mail.ru>
2019-11-05 13:33:51 +0300

    Ititial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+
+// JS - ./js/index.js
+import './js/'
+// SCSS
+import './scss/main.scss'
+// CSS (example)
+import './css/style.css'
+import './css/slick.css'
+
+// Bootstrap (example)
+// import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
+// import 'vue'
+// import Vue from 'vue'
+window.Vue = require('vue')
+
---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+// Main js file
+
+// another js file (example)
+import './main.js'
+import 'owl.carousel2/dist/assets/owl.carousel.css'
+import 'owl.carousel2'
+import 'slick-carousel'
+import 'slick-carousel/slick/slick.css'
+import 'font-awesome/css/font-awesome.css'
